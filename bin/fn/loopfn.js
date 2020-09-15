8935614d1090d08c35b43b2318d117d08381d126
thanaha <m20471chalunton@gmail.com>
2020-09-15 21:17:11 +0700

    fix

--- bin/fn/loopfn.js
+++ bin/fn/loopfn.js
@@ -41,7 +41,7 @@ module.exports.tt = async (req, res) => {
 
       agent.handleRequest(intentMap);
       const ans = agent.responseMessages_;
-      if(ans == undefined){
+      if(ans[0].text == undefined){
         ans = 'เอ๋ น้องไม่รู้'
       }
       const qtt = `INSERT INTO history(intent,question,answer)
