6e943f18863b6c1d452a3f6dc039e84826277a17
thanaha <m20471chalunton@gmail.com>
2020-09-15 20:43:10 +0700

    makt

--- bin/config/db.js
+++ bin/config/db.js
@@ -1,10 +1,10 @@
 var mysql = require('mysql');
 
 var db = mysql.createConnection({
-  host     : 'heroku_44e8d37e9d01440',
+  host     : 'us-cdbr-east-02.cleardb.com',
   user     : 'bde278f5d67f80',
-  password : '54565268',
-  database : 'dftbot'
+  password : 'f70832a88aa0dd5',
+  database : 'heroku_44e8d37e9d01440'
 });
  
 db.connect((err) => {
