408e79dcabd114ff20ad31ae082a0b39ad058a34
thanaha <m20471chalunton@gmail.com>
2020-09-16 10:23:21 +0700

    fix bug and change db

--- bin/config/db.js
+++ bin/config/db.js
@@ -1,15 +1,15 @@
 var mysql = require('mysql');
 
 var db = mysql.createConnection({
-  // host     : 'us-cdbr-east-02.cleardb.com',
-  // user     : 'bde278f5d67f80',
-  // password : 'f70832a88aa0dd5',
-  // database : 'heroku_44e8d37e9d01440'
+  host     : 'us-cdbr-east-02.cleardb.com',
+  user     : 'bde278f5d67f80',
+  password : 'f70832a88aa0dd5',
+  database : 'heroku_44e8d37e9d01440'
 
-  host     : 'localhost',
-  user     : 'root',
-  password : '123456',
-  database : 'dftbot'
+  // host     : 'localhost',
+  // user     : 'root',
+  // password : '123456',
+  // database : 'dftbot'
   
 });
  
